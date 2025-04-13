06881602ce862b3d8af70f90d05265219d0a9d89
cyzuko <mateus23viana@gmail.com>
2025-04-13 19:58:23 +0100

    intro

---
+++ app/src/main/java/com/example/tripsync/IntroAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.tripsync
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class IntroAdapter(private val items: List<IntroItem>) : RecyclerView.Adapter<IntroAdapter.IntroViewHolder>() {
+
+    class IntroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val image = view.findViewById<ImageView>(R.id.imageView)
+
+
+        fun bind(item: IntroItem) {
+            image.setImageResource(item.imageRes)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.intro_item, parent, false)
+        return IntroViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IntroViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+}
+
+data class IntroItem(val imageRes: Int, val title: String)
